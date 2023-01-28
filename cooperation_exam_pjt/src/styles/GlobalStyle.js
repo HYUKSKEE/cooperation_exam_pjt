4eac046b468b0bdcef1ed3601fb44d62d6fd32df
HYUKSKEE <gin280833@gmail.com>
2023-01-28 16:55:18 +0900

    fix : remove unused css code in GlobalStyle.js

--- cooperation_exam_pjt/src/styles/GlobalStyle.js
+++ cooperation_exam_pjt/src/styles/GlobalStyle.js
@@ -5,16 +5,7 @@ const GlobalStyle = createGlobalStyle`
   ${reset}
   
   * { box-sizing: border-box }
-  .react-datepicker{
-    width: 100%;
-    display: flex;
-    justify-content: space-between;
-    ;
-    
-    .react-datepicker__month-container{
-      width: 100%;
-    }
-  }
+  
 `;
 
 export default GlobalStyle;
