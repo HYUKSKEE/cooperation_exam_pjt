f53ac031c82cde763660a4707a585c490449bc21
HYUKSKEE <gin280833@gmail.com>
2023-01-30 21:29:22 +0900

    add : Exam.js html structure and css

--- cooperation_exam_pjt/src/pages/Exam/Exam.js
+++ cooperation_exam_pjt/src/pages/Exam/Exam.js
@@ -1,16 +1,40 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 function Exam() {
-  useEffect(() => {}, []);
+  const [examList, setExamList] = useState([]);
+
+  useEffect(() => {
+    fetch('/data/data.json')
+      .then(res => res.json())
+      .then(setExamList);
+  }, []);
   return (
     <Container>
       <Wrap>
-        <Question>Question</Question>
-        <Answer>dd</Answer>
+        {/* {examList.map(list => {
+          return (
+            <>
+              <Question>{list.question}</Question>
+              {list.answer.map(answer => {
+                return <Answer key={answer.id}>{answer.text}</Answer>;
+              })}
+            </>
+          );
+        })} */}
+
+        {/* test html structure */}
+        <Question>{examList?.[0]?.question}</Question>
+        {examList?.[0]?.answer.map(answer => {
+          return (
+            <Answer key={answer.id}>
+              {answer.id}. {answer.text}
+            </Answer>
+          );
+        })}
         <ButtonBox>
-          <Btn>dd</Btn>
-          <Btn>dd</Btn>
+          <Btn>{`< PREV`}</Btn>
+          <Btn>{`NEXT >`}</Btn>
         </ButtonBox>
       </Wrap>
     </Container>
@@ -19,9 +43,57 @@ function Exam() {
 
 export default Exam;
 
-const Container = styled.div``;
-const Wrap = styled.div``;
-const Question = styled.div``;
-const Answer = styled.button``;
-const ButtonBox = styled.div``;
-const Btn = styled.button``;
+const Container = styled.div`
+  width: 40%;
+  border-radius: 44px;
+  background-color: #ebe9e9;
+`;
+const Wrap = styled.div`
+  width: 80%;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`;
+const Question = styled.div`
+  margin: 20px 0px 20px 0px;
+`;
+const Answer = styled.button`
+  margin: 0px 0px 10px 0px;
+  padding: 0px 0px 0px 10px;
+  width: 100%;
+  height: 35px;
+  text-align: left;
+  border: none;
+  border-radius: 10px;
+  color: white;
+  background-color: orange;
+  cursor: pointer;
+
+  :hover {
+    transform: scale(1.05);
+  }
+`;
+const ButtonBox = styled.div`
+  width: 100%;
+  margin: 10px 0px 20px 0px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+const Btn = styled.button`
+  margin: 10px 0px 20px 0px;
+  padding: 0px 10px 0px 10px;
+  height: 30px;
+  text-align: left;
+  border: none;
+  border-radius: 10px;
+  color: white;
+  background-color: #2b5ff1;
+  cursor: pointer;
+
+  :hover {
+    transform: scale(1.05);
+  }
+`;
